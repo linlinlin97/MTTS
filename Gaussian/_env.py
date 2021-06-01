58c2740f2cf09e65655f153b75a96990e6cc5c4f
linlinlin97 <75768141+linlinlin97@users.noreply.github.com>
2021-05-31 20:17:15 -0400

    Update _env.py

--- Gaussian/_env.py
+++ Gaussian/_env.py
@@ -15,18 +15,15 @@ class Environment():
         
         self.theta = np.random.multivariate_normal(u_theta, Sigma_theta, 1)[0]
 
-#         self.simu_X(N)
         self.get_Phi(N, p, K)
         self.get_r(K, N, Sigma_delta)
         self.optimal_arms = np.argmax(self.r, 1) # N-dim
         self.errors = randn(N, T, K) * sigma
-#         self.simu_errors(N, T, K, sigma)
                 
          
     def get_Phi(self, N, p, K):
         """ consider the simple case now 
         [N, K, p]
-        TODO
         """
         np.random.seed(self.seed)
         if self.with_intercept:
@@ -35,24 +32,12 @@ class Environment():
             self.Phi = np.concatenate([self.intercept_Phi, self.random_part_Phi], axis = 2)
         else:
             self.Phi = np.random.multivariate_normal(self.X_mu, self.X_Sigma, (N, K))
-
-#         self.X_mu = np.zeros(p - K)
-#         self.X_Sigma = identity(p - K)
-#         # [N, K, p]
-#         self.intercept_Phi = np.repeat(identity(K)[np.newaxis, :, :], N, axis = 0)
-#         self.random_part_Phi = np.random.multivariate_normal(self.X_mu, self.X_Sigma, (N, K))
-#         self.Phi = np.concatenate([self.intercept_Phi, self.random_part_Phi], axis = 2)
-        
-#         self.X_mu = np.zeros(p)
-#         self.X_Sigma = identity(p)
-#         self.Phi = np.random.multivariate_normal(self.X_mu, self.X_Sigma, (N, K))
     
     def get_r(self, K, N, Sigma_delta):
         """ 
-        TODO : misspecifications can be added here. nonlinear as the true model to show the robustness w.r.t. LMM 
+        misspecifications can be added here. nonlinear as the true model to show the robustness w.r.t. LMM 
         """
         np.random.seed(self.seed)
-        # [N, K]
         self.deltas = np.random.multivariate_normal(np.zeros(K), Sigma_delta, N)
         self.r_mean = [Phi_i.dot(self.theta) for Phi_i in self.Phi]
         if self.misspecification is not None:
@@ -72,18 +57,7 @@ class Environment():
     def get_optimal_reward(self, i, t):
         a = self.optimal_arms[i]
         return (self.r[i] + self.errors[i, t])[a]
-#     def simu_errors(self, N, T, K, sigma):
-#         self.errors = randn(N, T, K) * sigma
 
-#     def approximate_linear_with_sin(self, x, max_x):
-#         """
-#         sin is close to x between [-pi / 2, pi / 2]
-#         1. x back to this range
-#         2. get the value of sin(x)
-#         3. transform back
-#         """
-#         factor = 1 / max_x * np.pi / 2
-#         return np.sin(x * factor) / factor
     def approximate_linear_with_cos(self, x, max_x):
         """
         sin is close to x between [-pi / 2, pi / 2]
